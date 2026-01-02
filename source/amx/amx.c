NexiusTailer/compiler

commit e5b7d1aa688eeb376d92b5606f2a5b9a68c47fae

Fix amx_PushStringLen length.

source/amx/amx.c
@@ -1778,7 +1778,7 @@ int AMXAPI amx_PushStringLen(AMX* amx, cell* amx_addr, cell** phys_addr, const c
 
     assert(amx != NULL);
 
-    numcells = length;
+    numcells = length + 1;
 
     if (pack)
         numcells = (numcells + sizeof(cell) - 1) / sizeof(cell);
@@ -1803,7 +1803,7 @@ int AMXAPI amx_PushString(AMX *amx, cell *amx_addr, cell **phys_addr, const char
   #if defined AMX_ANSIONLY
     length = strlen(string) + 1;
   #else
-    length = (use_wchar ? wcslen((const wchar_t*)string) : strlen(string)) + 1;
+    length = (use_wchar ? wcslen((const wchar_t*)string) : strlen(string));
   #endif
     return amx_PushStringLen(amx, amx_addr, phys_addr, string, length, pack, use_wchar);
 }

commit e56c0a718fffce95ae89c1a2f645428f02d86092

Fix for inconsistent length calculating
the bug was introduced in commit e5b7d1a

source/amx/amx.c
@@ -1775,7 +1775,7 @@ int AMXAPI amx_PushString(AMX *amx, cell *amx_addr, cell **phys_addr, const char
   assert(string!=NULL);
 
   #if defined AMX_ANSIONLY
-    length = strlen(string) + 1;
+    length = strlen(string);
   #else
     length = (use_wchar ? wcslen((const wchar_t*)string) : strlen(string));
   #endif